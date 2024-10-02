853ef30d04d2511bfb805fcea9bce52d5cc8c6d6
Luan Paredes <luan.developer.ti@gmail.com>
2024-10-02 17:40:12 -0300

    KB-003: Theme Working - missing settings yet

---
+++ Services/Classes/ThemeService.cs
@@ -0,0 +1,35 @@
+﻿using KanBoard.Services.Interfaces;
+using Microsoft.UI.Xaml;
+
+namespace KanBoard.Services.Classes
+{
+    public class ThemeService : IThemeService
+    {
+        public void DarkTheme()
+        {
+            ChangeAppTheme(ElementTheme.Dark);
+        }
+
+        public void HighContrastTheme()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void LightTheme()
+        {
+            ChangeAppTheme(ElementTheme.Light);
+        }
+
+        public void ChangeAppTheme(ElementTheme theme)
+        {
+            var dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
+
+            dispatcherQueue.TryEnqueue(
+                Microsoft.UI.Dispatching.DispatcherQueuePriority.Normal,
+                () =>
+                {
+                    App.Instance.ChangeTheme(theme);
+                });
+        }
+    }
+}
