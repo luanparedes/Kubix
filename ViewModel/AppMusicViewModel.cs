b8781d16d7e7131f010fcf42eff6b0d7edbce917
Luan Paredes <luan.developer.ti@gmail.com>
2024-10-09 01:32:45 -0300

    KB-007: Implementation Visual State

--- ViewModel/AppMusicViewModel.cs
+++ ViewModel/AppMusicViewModel.cs
@@ -6,35 +6,31 @@ using CommunityToolkit.Mvvm.ComponentModel;
 namespace KanBoard.ViewModel
 {
     public class AppMusicViewModel : ObservableObject
-    {       
+    {
+        #region Constants
+
         public readonly string SpotifyURL = "http://www.spotify.com";
         public readonly string DeezerURL = "http://deezer.com";
 
+        #endregion
+
+        public string CurrentState { get; set; } = "ChoiceAppState";
+
         public MusicApp ActualMusicApp;
 
-        StackPanel stackPanel;
+        Control pageControl;
         WebView2 webView;
-        Button spotifyButtonBar;
-        Button deezerButtonBar;
-
-        public void StackPanel_Loaded(object sender, RoutedEventArgs e)
-        {
-            stackPanel = sender as StackPanel;
-        }
 
         public void WebView2_Loaded(object sender, RoutedEventArgs e)
         {
             webView = sender as WebView2;
         }
 
-        public void SpotifyButtonBar_Loaded(object sender, RoutedEventArgs e)
+        public void PageControl_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
         {
-            spotifyButtonBar = sender as Button;
-        }
+            pageControl = sender as UserControl;
+            VisualStateManager.GoToState(pageControl, CurrentState, true);
 
-        public void DeezerButtonBar_Loaded(object sender, RoutedEventArgs e)
-        {
-            deezerButtonBar = sender as Button;
         }
 
         public void Button_Click(object sender, RoutedEventArgs e)
@@ -46,32 +42,19 @@ namespace KanBoard.ViewModel
                 case "SpotifyBtn":
                     webView.Source = new Uri(SpotifyURL);
                     ActualMusicApp = MusicApp.Spotify;
+                    CurrentState = "SpotifyAppState";
                     break;
                 case "DeezerBtn":
                     webView.Source = new Uri(DeezerURL);
                     ActualMusicApp = MusicApp.Deezer;
+                    CurrentState = "DeezerAppState";
+                    break;
+                case "BackButton":
+                    CurrentState = "ChoiceAppState";
                     break;
             }
 
-            ChangePageState();
-        }
-
-        private void ChangePageState()
-        {
-            stackPanel.Visibility = Visibility.Collapsed;
-            webView.Visibility = Visibility.Visible;
-
-            if (ActualMusicApp == MusicApp.Spotify)
-            {
-                spotifyButtonBar.Visibility = Visibility.Collapsed;
-                deezerButtonBar.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                spotifyButtonBar.Visibility = Visibility.Visible;
-                deezerButtonBar.Visibility = Visibility.Collapsed;
-            }
-                
+            VisualStateManager.GoToState(pageControl, CurrentState, true);
         }
     }
 
