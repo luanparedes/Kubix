c4f7bccbda3283b1a6955daac40d0381d153fcca
Luan Paredes <luan.developer.ti@gmail.com>
2024-10-12 04:06:56 -0300

    KB-011: Notepad control created, under implementation

---
+++ Controls/NotepadControl.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+
+namespace KanBoard.Controls
+{
+    class NotepadControl : Control
+    {
+        #region Fields & Properties
+
+        private TabView customTabView;
+
+        public event EventHandler<SelectionChangedEventArgs> NoteSelectionChange;
+        public event EventHandler<TabViewItem> NoteTextChanged;
+
+        #endregion
+
+        #region Methods
+
+        private TabViewItem AddNewtab()
+        {
+            TextBox textBox = new TextBox();
+            textBox.Style = (Style)App.Current.Resources["NotepadTextBox"];
+            textBox.TextChanged += TextBox_TextChanged;
+
+            TabViewItem tabItem = new TabViewItem();
+            tabItem.Header = "New";
+            tabItem.Content = textBox;
+
+            return tabItem;
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void CustomTabView_AddTabButtonClick(TabView sender, object args)
+        {
+            customTabView.TabItems.Add(AddNewtab());
+        }
+
+        private void CustomTabView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            NoteSelectionChange?.Invoke(this, e);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TabViewItem tabItem = (sender as TextBox).Parent as TabViewItem;
+            NoteTextChanged?.Invoke(sender, tabItem);
+        }
+
+        #endregion
+
+        #region OnApplyTemplate
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            customTabView = GetTemplateChild("TabViewCustom") as TabView;
+
+            if (customTabView != null)
+            {
+                customTabView.SelectionChanged += CustomTabView_SelectionChanged;
+                customTabView.AddTabButtonClick += CustomTabView_AddTabButtonClick;
+
+                customTabView.TabItems.Add(AddNewtab());
+                customTabView.SelectedIndex = 0;
+            }
+        }
+
+        #endregion
+    }
+}
--- ViewModel/NotepadViewModel.cs
+++ ViewModel/NotepadViewModel.cs
@@ -1,8 +1,100 @@
 ﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
 namespace KanBoard.ViewModel
 {
-    public class NotepadViewModel
+    public class NotepadViewModel : ObservableObject
     {
+        #region Fields & Properties
 
+        private string NotepadText {  get; set; }
+        private StorageFile CurrentFile { get; set; }
+
+        private bool _hasTextChange = false;
+        public bool HasTextChange
+        {
+            get { return _hasTextChange; }
+            set { SetProperty(ref _hasTextChange, value); }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private async void OpenFile()
+        {
+            var openPicker = new FileOpenPicker();
+            openPicker.SuggestedStartLocation = PickerLocationId.ComputerFolder;
+            openPicker.FileTypeFilter.Add(".txt");
+
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.Instance.MainWindow);
+            WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hwnd);
+
+            StorageFile file = await openPicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                CurrentFile = file;
+                NotepadText = await FileIO.ReadTextAsync(file);
+            }
+        }
+
+        private async void SaveFile()
+        {
+            var savePicker = new FileSavePicker();
+
+            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            savePicker.FileTypeChoices.Add("Text File", new List<string>() { ".txt" });
+            savePicker.SuggestedFileName = "New Document";
+
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.Instance.MainWindow);  // Obtém o handle da janela principal
+            WinRT.Interop.InitializeWithWindow.Initialize(savePicker, hwnd);
+
+            StorageFile file = await savePicker.PickSaveFileAsync();
+
+            if (file != null)
+            {
+                await FileIO.WriteTextAsync(file, NotepadText);
+            }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        public void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+        }
+
+        public void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFile();
+        }
+
+        public void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            NotepadText = (sender as TextBox).Text;
+            HasTextChange = true;
+        }
+
+        public void NotepadControl_NoteSelectionChange(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        public void NotepadControl_NoteTextChanged(object sender, TabViewItem e)
+        {
+            NotepadText = (sender as TextBox).Text;
+            HasTextChange = true;
+        }
+
+        #endregion
     }
 }
