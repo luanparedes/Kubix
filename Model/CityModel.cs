8acfdfd0d6389f8b41ec7f592fbc307f6e66b718
Luan Paredes <luan.developer.ti@gmail.com>
2024-11-16 04:09:49 -0300

    Working on Weather Forecast - Get City working

---
+++ Model/CityModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Kubix.Model
+{
+    public class CityModel
+    {
+        public string City { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string Country { get; set; }
+
+        public string State { get; set; }
+
+        public string Population { get; set; }
+    }
+}
---
+++ Services/Interfaces/IExcelService.cs
@@ -0,0 +1,15 @@
+﻿using Kubix.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kubix.Services.Interfaces
+{
+    public interface IExcelService
+    {
+        public List<CityModel> GetTypedCities(string text);
+        public CityModel GetCityByPosition(double latitude, double longitude);
+    }
+}
