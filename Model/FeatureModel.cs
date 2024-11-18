1b3270697ac0a28cebe41c0d5b345b06e9014e83
Luan Paredes <luan.developer.ti@gmail.com>
2024-11-18 02:31:32 -0300

    KB-033: Implemented data template to items menu

---
+++ Model/FeatureModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml.Media.Imaging;
+
+namespace Kubix.Model
+{
+    public class FeatureModel
+    {
+        public string FeatureName { get; set; }
+        public BitmapImage FeatureIcon { get; set; }
+        public string FeatureAlias { get; set; }
+    }
+}
--- ViewModel/MainBoardViewModel.cs
+++ ViewModel/MainBoardViewModel.cs
@@ -2,10 +2,14 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.DependencyInjection;
 using Kubix.Helpers;
+using Kubix.Model;
 using Kubix.Services.Interfaces;
 using Kubix.View;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+using System.Collections.Generic;
 
 namespace Kubix.ViewModel
 {
@@ -47,6 +51,8 @@ namespace Kubix.ViewModel
         private readonly INavigationService _navigationService = Ioc.Default.GetService<INavigationService>();
         private readonly ILogger _logger = Ioc.Default.GetService<ILogger>();
 
+        public List<FeatureModel> FeaturesList;
+
         #endregion
 
         #region Contructor
@@ -55,6 +61,7 @@ namespace Kubix.ViewModel
         {
             _logger.InfoLog("Entered Constructor ViewModel!");
             _dataInitial.UIUpdateChanged += _dataInitial_UIUpdateChanged;
+
             GetChoicesFeatures();
         }
 
@@ -76,64 +83,156 @@ namespace Kubix.ViewModel
             IsCompilersShowing = _dataInitial.HasCompilers;
         }
 
+        private List<FeatureModel> GetAllFeatures()
+        {
+            List<FeatureModel> featuresList = new List<FeatureModel>();
+
+            featuresList.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_HomeText"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/home_feature.png")),
+                FeatureAlias = "HomeId",
+            });
+
+            featuresList.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_WebBrowserText"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/globe_feature.png")),
+                FeatureAlias = "BrowserId",
+            });
+
+            featuresList.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_AIText"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/ai2_feature.png")),
+                FeatureAlias = "AiId",
+            });
+
+            featuresList.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_MusicText"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/music_feature.png")),
+                FeatureAlias = "MusicId",
+            });
+
+            featuresList.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_YoutubeText"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/youtube_feature.png")),
+                FeatureAlias = "YoutubeId",
+            });
+
+            featuresList.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_StreamingText"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/streaming_feature.png")),
+                FeatureAlias = "StreamingId",
+            });
+
+            featuresList.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_SocialMediaText"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/socialmedia_feature.png")),
+                FeatureAlias = "SocialMediaId",
+            });
+
+            featuresList.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_KNoteText"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/note_feature.png")),
+                FeatureAlias = "KNoteId",
+            });
+
+            featuresList.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_Office365Text"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/office_feature.png")),
+                FeatureAlias = "OfficeId",
+            });
+
+            featuresList.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_GoogleText"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/google_feature.png")),
+                FeatureAlias = "GoogleId",
+            });
+
+            featuresList.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_CompilersText"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/compiler_feature.png")),
+                FeatureAlias = "CompilersId",
+            });
+
+            return featuresList;
+        }
+
         #endregion
 
         #region Event Handlers
+
+        //TODO: Agora perdeu a referencia entre a choices page, settings e menu
+        //após ter sido excluído os items.
 
         public void NavigationView_Loaded(object sender, RoutedEventArgs e)
         {
             NavigationView navigationView = sender as NavigationView;
 
+            FeaturesList = GetAllFeatures();
+            navigationView.MenuItemsSource = FeaturesList;
+            navigationView.FooterMenuItems.Add(new FeatureModel()
+            {
+                FeatureName = Stringer.GetString("KB_SettingsText"),
+                FeatureIcon = new BitmapImage(new Uri($"ms-appx:///Assets/settings_feature.png")),
+                FeatureAlias = "SettingsId",
+            });
+
             _navigationService.SetFrame((Frame)(sender as NavigationView).Content, FrameTypeEnum.NavigationViewFrame);
-            navigationView.SelectedItem = navigationView.MenuItems[0];
+            navigationView.SelectedItem = (navigationView.MenuItemsSource as List<FeatureModel>)[0];
         }
 
         public void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
             Frame frame = (sender.Content) as Frame;
 
-            var selectedItem = args.SelectedItem as NavigationViewItem;
-            var tag = (string)selectedItem.Tag;
+            string alias = (args.SelectedItem as FeatureModel).FeatureAlias;
 
-            switch(tag)
+            switch(alias)
             {
-                case "HomePage":
+                case "HomeId":
                     _navigationService.GoToNavigationView(typeof(HomePage));
                     break;
-                case "SettingsPage":
+                case "SettingsId":
                     _navigationService.GoToNavigationView(typeof(SettingsPage));
                     break;
-                case "UserInfoPage":
-                    _navigationService.GoToNavigationView(typeof(UserInfoPage));
-                    break;
-                case "AppMusicPage":
+                case "MusicId":
                     _navigationService.GoToNavigationView(typeof(AppMusicPage));
                     break;
-                case "BrowserPage":
+                case "BrowserId":
                     _navigationService.GoToNavigationView(typeof(BrowserPage));
                     break;
-                case "YoutubePage":
+                case "YoutubeId":
                     _navigationService.GoToNavigationView(typeof(YoutubePage));
                     break;
-                case "StreamingsPage":
+                case "StreamingId":
                     _navigationService.GoToNavigationView(typeof(StreamingsPage));
                     break;
-                case "NotepadPage":
+                case "KNoteId":
                     _navigationService.GoToNavigationView(typeof(KNotePage));
                     break;
-                case "AIPage":
+                case "AiId":
                     _navigationService.GoToNavigationView(typeof(AIPage));
                     break;
-                case "Office365Page":
+                case "OfficeId":
                     _navigationService.GoToNavigationView(typeof(Office365Page));
                     break;
-                case "GooglePage":
+                case "GoogleId":
                     _navigationService.GoToNavigationView(typeof(GooglePage));
                     break;
-                case "SocialMediaPage":
+                case "SocialMediaId":
                     _navigationService.GoToNavigationView(typeof(SocialMediasPage));
                     break;
-                case "CompilersPage":
+                case "CompilersId":
                     _navigationService.GoToNavigationView(typeof(CompilersPage));
                     break;
             }
